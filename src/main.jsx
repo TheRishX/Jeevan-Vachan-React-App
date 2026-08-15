a2e12feada5a993e8186c006f658591cfa4f307c
Rishabh kumar <therishx@gmail.com>
2026-08-15 08:26:49 +0545

    v1.2

--- src/main.jsx
+++ src/main.jsx
@@ -4,4 +4,5 @@ import App from './App';
 import './styles.css';
 import './themes.css';
 import './reader-enhancements.css';
+import './media-enhancements.css';
 createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
