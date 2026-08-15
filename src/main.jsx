dd330edf023eaf061e42a5bab35cd4a8f4c22952
Rishabh kumar <therishx@gmail.com>
2026-08-15 08:17:50 +0545

    Bible Project V.1.1

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,5 @@ import {createRoot} from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './themes.css';
+import './reader-enhancements.css';
 createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
